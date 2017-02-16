f26c7b1067393bc6ed39b54f6bcef002fbd745ee
William Flores <w.f.08@live.com>
2017-02-15 23:41:30 -0800

    Added box

--- superset/assets/visualizations/main.js
+++ superset/assets/visualizations/main.js
@@ -30,5 +30,6 @@ const vizMap = {
   word_cloud: require('./word_cloud.js'),
   world_map: require('./world_map.js'),
   dual_line: require('./nvd3_vis.js'),
+  make_box: requie('./make_box.js')
 };
 export default vizMap;
---
+++ superset/assets/visualizations/make_box.js
@@ -0,0 +1,8 @@
+const $ = require('jquery');
+
+function makeBox(slice, payload) {
+  console.log(slice)
+  console.log(payload)
+}
+
+module.exports = makeBox
